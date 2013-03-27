5a7c7826602e03dd34056f06cf70d84460600472
Shohei OKADA <okashoicircus0@gmail.com>
2013-03-27 23:14:50 +0900

    first commit

---
+++ NounPhrase.py
@@ -0,0 +1,55 @@
+﻿# !/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# for python 2.x
+
+
+def extractor(fin):
+    nounphrase = u""
+    for line in fin:
+        uline = unicode(line.strip(), "utf-8")
+        if uline.startswith(u"* ") or uline.startswith(u"EOF"):
+            if nounphrase:
+                yield nounphrase
+                nounphrase = u""
+            continue
+
+        try:
+            surface, attribute = uline.split(u"\t")[:2]
+            pos = attribute.split(u",")[0]
+        except ValueError:
+            if nounphrase:
+                yield nounphrase
+                nounphrase = u""
+            continue
+
+        if pos != u"名詞":
+            if nounphrase:
+                yield nounphrase
+                nounphrase = u""
+            continue
+
+        nounphrase += surface
+
+
+def _main(args):
+    import sys
+
+    try:
+        fin = open(args[1])
+    except IndexError:
+        sys.stderr.write("Error: Missing filename\n")
+        sys.stderr.write("Usage: python NounPhrase.py filename\n")
+        return -1
+    except IOError:
+        sys.stderr.write("Error: The file doesn't exist")
+        return -1
+
+    for np in extractor(fin):
+        print np
+
+    return 0
+
+if __name__ == "__main__":
+    import sys
+    sys.exit(_main(sys.argv))
---
+++ SentenceSegmentator.py
@@ -0,0 +1,36 @@
+﻿# !/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# for python 2.x
+
+
+def getSentences(fin):
+    for line in fin:
+        uline = unicode(line.strip(), "utf-8")
+        if not uline:
+            continue
+        for sentence in uline.split((u"。")):
+            if not sentence:
+                continue
+            yield sentence
+
+
+def _main(args):
+    import sys
+    try:
+        fin = open(args[1])
+    except IndexError:
+        sys.stderr.write("Error: Missing filename\n")
+        sys.stderr.write("Usage: python SenteceSegmentator.py filename")
+        return -1
+    except IOError:
+        sys.stderr.write("Error: The file doesn't exist")
+        return -1
+
+    for sentence in getSentences(fin):
+        print sentence
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(_main(sys.argv))
