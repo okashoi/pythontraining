8ff47720b34a70bbcfec48a0bf8380da0e499cc5
Shohei OKADA <okashoicircus0@gmail.com>
2013-04-05 05:05:27 +0900

    Grouping nounphrases and print it in CaboCha's format instead printing only nounphrases.

--- NounPhrase.py
+++ NounPhrase.py
@@ -1,32 +1,31 @@
-﻿# !/usr/bin/env python2.7
+# !/usr/bin/env python2.7
 # -*- coding: utf-8 -*-
 
 # for python 2.x
 
 
-def extractor(fin):
+def group(fin):
     nounphrase = u""
     for line in fin:
         uline = unicode(line.strip(), "utf-8")
         if uline.startswith(u"* ") or uline.startswith(u"EOF"):
             if nounphrase:
-                yield nounphrase
+                yield nounphrase + u"/t名詞句,*,*,*,*,*,*,*,*\t0"
                 nounphrase = u""
+            yield uline
             continue
 
         try:
             surface, attribute = uline.split(u"\t")[:2]
             pos = attribute.split(u",")[0]
         except ValueError:
-            if nounphrase:
-                yield nounphrase
-                nounphrase = u""
             continue
 
         if pos != u"名詞":
             if nounphrase:
-                yield nounphrase
+                yield nounphrase + u"\t名詞句,*,*,*,*,*,*,*,*\t0"
                 nounphrase = u""
+            yield uline
             continue
 
         nounphrase += surface
@@ -45,8 +44,10 @@ def _main(args):
         sys.stderr.write("Error: The file doesn't exist")
         return -1
 
-    for np in extractor(fin):
-        print np
+    for uline in group(fin):
+        print uline.encode("utf-8")
+
+    fin.close()
 
     return 0
 
